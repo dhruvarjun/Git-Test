95dcea988a6a3f346b55a2ec313ccad1740debd2
dhruvarjun <deepmani@shaw.ca>
2014-04-12 00:24:57 -0700

    added a test class

---
+++ Git Test/DDTest.h
@@ -0,0 +1,13 @@
+//
+//  DDTest.h
+//  Git Test
+//
+//  Created by Administrator on 2014-04-12.
+//  Copyright (c) 2014 Dhruv Handa. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DDTest : NSObject
+
+@end
---
+++ Git Test/DDTest.m
@@ -0,0 +1,13 @@
+//
+//  DDTest.m
+//  Git Test
+//
+//  Created by Administrator on 2014-04-12.
+//  Copyright (c) 2014 Dhruv Handa. All rights reserved.
+//
+
+#import "DDTest.h"
+
+@implementation DDTest
+
+@end
